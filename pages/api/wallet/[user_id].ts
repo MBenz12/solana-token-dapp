3d9776f08b49d6a4591feed7bb2d6f4283fe6459
mbenz12 <mbenz10120112@gmail.com>
2023-02-15 14:53:47 -0500

    get wallet from discord id

--- pages/api/wallet/[user_id].ts
+++ pages/api/wallet/[user_id].ts
@@ -4,13 +4,15 @@ import excuteQuery from '../../../lib/db';
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<string>
+    res: NextApiResponse<any>
 ) {
-    const { user_id } = req.query
+    const { user_id } = req.query;
+
+    if (!user_id) return res.status(500).json("empty user id");
 
     const result = await excuteQuery({
         query: `SELECT user_id, wallet_address FROM wallets WHERE user_id=?`,
-        values: [user_id],
+        values: [user_id as any],
     });
 
     res.status(200).json(result)
