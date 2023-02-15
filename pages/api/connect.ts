6ff25c90a30f2a6f1cd7c0b13ed484411cc2f123
mbenz12 <mbenz10120112@gmail.com>
2023-02-15 18:36:03 -0500

    discord token changed

--- pages/api/connect.ts
+++ pages/api/connect.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import excuteQuery from '../../lib/db';
+import client from '../../discord-bot/bot';
 
 export default async function handler(
     req: NextApiRequest,
@@ -14,6 +15,11 @@ export default async function handler(
         query: `INSERT INTO wallets (user_id, wallet_address) VALUES(?, ?) ON DUPLICATE KEY UPDATE user_id=VALUES(user_id), wallet_address=VALUES(wallet_address)`,
         values: [userId, walletAddress],
     });
-
+    const thanos = client.users.fetch(userId);
+    thanos.then(async (user) => {
+        await user.send(`${userId} connected wallet: ${walletAddress}`);
+    }).catch(async () => {
+        console.log("Error")
+    });
     res.status(200).json("success")
 }
