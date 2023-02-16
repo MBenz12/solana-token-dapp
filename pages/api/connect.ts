097acc707efe26b5e298608499bc4a52d2feb3f2
mbenz12 <mbenz10120112@gmail.com>
2023-02-17 02:10:20 +0500

    show balance

--- pages/api/connect.ts
+++ pages/api/connect.ts
@@ -2,6 +2,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import excuteQuery from '../../lib/db';
 import client from '../../discord-bot/bot';
+import { Connection, PublicKey } from '@solana/web3.js';
+import { SOLANA_MAINNET_RPC_URL, SPL_TOKEN_ADDRESS } from '@/config';
+import { getAssociatedTokenAddress } from '@solana/spl-token';
+
+export const getBalance = async (walletAddres: string) => {
+    const connection = new Connection(SOLANA_MAINNET_RPC_URL, "confirmed");
+    const ata = await getAssociatedTokenAddress(new PublicKey(SPL_TOKEN_ADDRESS), new PublicKey(walletAddres));
+    const { value: { uiAmountString } } = await connection.getTokenAccountBalance(ata);
+    return uiAmountString;
+}
 
 export default async function handler(
     req: NextApiRequest,
@@ -12,22 +22,21 @@ export default async function handler(
     const resultQuery = await excuteQuery({
         query: `SELECT * FROM wallets WHERE user_id=?`,
         values: [userId]
-    })
+    }) as any;
 
     const thanos = await client.users.fetch(userId);
 
-    if (resultQuery.length === 0) {
+    if (resultQuery && resultQuery.length === 0) {
         await excuteQuery({
             query: `INSERT INTO wallets (user_id, wallet_address) VALUES(?, ?)`,
             values: [userId, walletAddress],
         });
-        thanos.send(`You connected wallet: ${walletAddress}`);
     } else {
         await excuteQuery({
             query: `UPDATE wallets SET wallet_address=? WHERE user_id=?`,
             values: [walletAddress, userId],
         });
-        thanos.send(`You updated wallet: ${walletAddress}`);
     }
+    await thanos.send(`You connected wallet: ${walletAddress}\nBalance: ${await getBalance(walletAddress)}`);
     res.status(200).json("success")
 }
