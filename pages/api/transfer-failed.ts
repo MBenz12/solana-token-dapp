1e8cbdccef2be47aee2d44b8fc6c077df8b293bf
mbenz12 <mbenz10120112@gmail.com>
2023-03-15 08:37:36 -0400

    fixed execute query

--- pages/api/transfer-failed.ts
+++ pages/api/transfer-failed.ts
@@ -9,14 +9,21 @@ export default async function handler(
     res: NextApiResponse<string>
 ) {
     const { userId, targetUserId, amount } = req.body;
-    const result = await excuteQuery({
+    const result1 = await excuteQuery({
         query: `SELECT wallet_address FROM wallets WHERE user_id=?`,
-        values: [userId, targetUserId],
+        values: [userId],
     }) as any;
+    const result2 = await excuteQuery({
+        query: `SELECT wallet_address FROM wallets WHERE user_id=?`,
+        values: [targetUserId],
+    }) as any;
+    if (result2.error) {
+        
+    }
     const thanos = await client.users.fetch(userId);
     const targetThanos = await client.users.fetch(targetUserId);
 
-    await thanos.send(`Failed to transfer ${amount} to <@${targetThanos.id}>\nBalance: ${await getBalance(result[0])}: ${JSON.stringify(result)}`);
+    await thanos.send(`Failed to transfer ${amount} to <@${targetThanos.id}>\nBalance: ${await getBalance(result1[0])}: ${JSON.stringify(result1) + JSON.stringify(result2)}`);
     // await targetThanos.send(`${amount} received from <@${thanos.id}>\nBalance: ${await getBalance(result[1])}`);
 
     res.status(200).json("success")
