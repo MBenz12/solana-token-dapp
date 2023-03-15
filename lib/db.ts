6d31bccacb8eb05bc5d0eb8860457b399c3f8830
mbenz12 <mbenz10120112@gmail.com>
2023-03-15 09:57:01 -0400

    changed error handling for db

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mysql from 'serverless-mysql';
 const db = mysql({
     config: {
         host: process.env.MYSQL_HOST,
-        port: parseInt(process.env.MYSQL_PORT || ''),
+        port: parseInt(process.env.MYSQL_PORT || '3306'),
         database: process.env.MYSQL_DATABASE,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD
@@ -16,7 +16,9 @@ type queryProps = {
 
 export default async function excuteQuery({ query }: queryProps) {
     try {
+        await db.connect();
         const results = await db.query(query);
+        console.log(results);
         await db.end();
         return results;
     } catch (error) {
