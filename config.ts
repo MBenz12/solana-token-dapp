4765d3c881765f42b5ba7cadccf7887b743ceda0
mbenz12 <mbenz10120112@gmail.com>
2023-03-15 08:02:19 -0400

    fixed config for sol

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const SOLANA_MAINNET_RPC_URL = process.env.SOLANA_MAINNET_RPC_URL || 'https://api.mainnet-beta.solana.com';
 export const SOLANA_DEVNET_RPC_URL = process.env.SOLANA_DEVNET_RPC_URL || 'https://api.devnet.solana.com';
-export const SPL_TOKEN_ADDRESS = process.env.SPL_TOKEN_ADDRESS || 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+export const SPL_TOKEN_ADDRESS = process.env.SPL_TOKEN_ADDRESS || '';
 
